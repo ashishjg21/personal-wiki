d5931569829bc79781cb95d807c43b685c442a52
ashishjg21 <ajg8188@gmail.com>
2023-03-28 23:55:36 +0530

    name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   // for home page 
-  title: 'Second brain',
-  tagline: 'I\'m so  cool',
+  title: 'My Second brain',
+  tagline: 'I\'m so cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
